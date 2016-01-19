dc02a4b00cc457f5e00dc83bcdc5a1865fbdeba3
rc0x03 <chipka01@email.franklin.edu>
2016-01-18 20:45:44 -0600

    Selector fixes and tests

--- last.js
+++
@@ -1,5 +0,0 @@
-var libxmljs = require('libxmljs');
-
-var doc = libxmljs.parseHtml('<div><img three/></div><img two/><img one/>');
-
-console.log(doc.find('/descendant::img[last()]')[0].toString())
